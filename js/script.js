865708d9be23cb7d4a03cd87e591224ac6c6e1c1
valentina cantarelli <valentinacanterina@gmail.com>
2021-09-01 16:32:24 +0200

    aggiunto evento click

--- js/script.js
+++ js/script.js
@@ -3,13 +3,21 @@
 // Al click su una cella dovrà essere mostrato con un alert il numero della cella e il suo background diventerà rosso.
 
 
-var numeroCelle=parseInt(prompt("Ciao,scegli il numero di celle tra 1 e 10"));
+var numeroCelle=parseInt(prompt("Ciao,scegli il numero di celle tra 1 e 100"));
+console.log(numeroCelle);
 
  function creazioneGriglia(num1){
-    for (var i=1;i<=numeroCelle.length;i++){
+    for (var i=1;i<=num1;i++){
         document.getElementById("campo").innerHTML+=`<div class="caselle">${i}</div>`;
     } 
-  }
+   }
 
   creazioneGriglia(numeroCelle);
 
+document.getElementById("campo").addEventListener("click",
+   function(event){
+       event.target.classList.toggle("caselle-click");
+       alert(event.target.innerHTML);
+   }
+
+);
